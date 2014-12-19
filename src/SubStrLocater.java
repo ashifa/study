5f8dcf0ae3e7c01905d112b4b2dfd14db048135e
305020571 <305020571@HCA-8SWF5R1.logon.ds.ge.com>
2014-12-19 13:12:56 +0800

    sub str

--- src/SubStrLocater.java
+++ src/SubStrLocater.java
@@ -1,24 +1,36 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class SubStrLocater {
 
 	public static void main(String[] args) {
 
-		String in = "adfdafdas";
-
-		char[] tmp = new char[50];
+		int abc = 6;
 		Random rd = new Random();
-
+		char[] tmp = new char[50];
 		for (int i = 0; i < tmp.length; i++)
-			tmp[i] = (char) (rd.nextInt(26) + 'a');
+			tmp[i] = (char) (rd.nextInt(abc) + 'a');
 
-		Arrays.sort(tmp);
+		char[] substr = new char[3];
+		for (int i = 0; i < substr.length; i++)
+			substr[i] = (char) (rd.nextInt(abc) + 'a');
+
+		// Arrays.sort(tmp);
 		System.out.println(tmp);
-		int a = 'A';
-		int b= 'a';
-		System.out.println(b);
-				
+
+		System.out.println(substr);
+
+		for (int i = 0; i < tmp.length - substr.length; i++) {
+			int j = 0;
+			for (j = 0; j < substr.length; j++) {
+				if (substr[j] != tmp[i + j])
+					break;
+			}
+			if (j == substr.length) {
+				System.out.println(i);
+				break;
+			}
+		}
+
 	}
 
 }
