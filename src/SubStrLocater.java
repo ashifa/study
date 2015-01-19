7fe01a7fc206866912164644f5411c8df5137514
305020571 <305020571@HCA-8SWF5R1.logon.ds.ge.com>
2015-01-19 14:39:27 +0800

    add test

--- src/SubStrLocater.java
+++ src/SubStrLocater.java
@@ -1,35 +1,48 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class SubStrLocater {
 
+	public static long locate(char[] str, char[] sub) {
+		for (int i = 0; i < str.length - sub.length + 1; i++) {
+			int j;
+			for (j = 0; j < sub.length; j++) {
+				if (str[i + j] == sub[j])
+					continue;
+				else
+					break;
+			}
+			if (j == sub.length)
+				return i;
+
+		}
+		return -1;
+	}
+
+	public static void test(char[] str, char[] sub) {
+		System.out.println(str);
+
+		System.out.println(sub);
+		
+		long a = locate(str, sub);
+		long b = String.valueOf(str).indexOf(String.valueOf(sub));
+ 
+		System.out.println(a+" "+b +" " +(a==b));
+	}
+
 	public static void main(String[] args) {
 
-		int abc = 6;
+		int abc = 3;
 		Random rd = new Random();
-		char[] tmp = new char[50];
+		char[] tmp = new char[6];
 		for (int i = 0; i < tmp.length; i++)
 			tmp[i] = (char) (rd.nextInt(abc) + 'a');
 
-		char[] substr = new char[3];
+		char[] substr = new char[2];
 		for (int i = 0; i < substr.length; i++)
 			substr[i] = (char) (rd.nextInt(abc) + 'a');
 
-		// Arrays.sort(tmp);
-		System.out.println(tmp);
-
-		System.out.println(substr);
-
-		for (int i = 0; i < tmp.length - substr.length; i++) {
-			int j = 0;
-			for (j = 0; j < substr.length; j++) {
-				if (substr[j] != tmp[i + j])
-					break;
-			}
-			if (j == substr.length) {
-				System.out.println(i);
-				break;
-			}
-		}
+		test(tmp,substr);
 
 	}
 
